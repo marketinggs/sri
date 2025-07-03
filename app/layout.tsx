b4eb5d5f9caef8ca058270f4f1ba176d8862e68b
marketinggs <marketing@growthschool.io>
2025-07-03 11:07:42 +0530

    Remove binary favicon

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const ibmPlexSans = localFont({
+  src: [
+    {
+      path: "../node_modules/@fontsource/ibm-plex-sans/files/ibm-plex-sans-latin-400-normal.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../node_modules/@fontsource/ibm-plex-sans/files/ibm-plex-sans-latin-500-normal.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../node_modules/@fontsource/ibm-plex-sans/files/ibm-plex-sans-latin-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-ibm-plex-sans",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Hello World App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={`${ibmPlexSans.variable} min-h-screen`}>{children}</body>
+    </html>
+  );
+}
